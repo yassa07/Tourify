e6ab013c90250f6982d92288e1686691581b4e0a
yassa07 <Y.Faheem68619@student.aast.edu>
2026-05-07 16:22:49 +0300

    project

---
+++ demo10/src/main/java/com/example/demo10/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.demo10;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(User.class, args);
+    }
+}
